5274736e8932d617eec647823010c3227dd8499e
Shubhodeep Mitra <mitra.shubhodeep@gmail.com>
2016-03-30 01:22:25 +0530

    Changed all the button methods to OnClickListener

--- app/src/main/java/studio/mitra/lightsoff/MainActivity.java
+++ app/src/main/java/studio/mitra/lightsoff/MainActivity.java
@@ -6,18 +6,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-
-
-        setContentView(R.layout.activity_main);
-    }
-    //--------------------------------------------------------------------------------
 
     /**
      * Global Variables
@@ -33,6 +25,144 @@ public class MainActivity extends AppCompatActivity {
     public static int period=1;
 
     //-----------------------------------------------------------------------------
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+        setContentView(R.layout.activity_main);
+        //-------------------------------------------------------------------------
+        /**
+         *
+         *Creating reference for Buttons
+         */
+
+        //Hour Button Decrement
+        Button hourDec=(Button)findViewById(R.id.hourButtonDec);
+
+        //Hour Button Increment
+
+        Button hourInc=(Button)findViewById(R.id.hourButtonInc);
+
+        //Minutes Button Increment
+
+        Button minDec=(Button)findViewById(R.id.minButtonDec);
+
+        //Minute Button Increment
+
+        Button minInc = (Button)findViewById(R.id.minButtonInc);
+
+        //AM|PM button
+        Button amPm=(Button) findViewById(R.id.amPmButton);
+
+        //------------------------------------------------------
+        /**
+         * Hours Button method
+         */
+
+        hourDec.setOnClickListener(
+                new Button.OnClickListener(){
+                    //decrement hours count
+                    public  void onClick(View view)
+                    {
+                        if(hours==1)
+                        {
+                            hours=12;
+                        }
+                        else
+                        {
+                            --hours;
+                        }
+                        displayHours(hours);
+                    }
+
+                }
+        );
+
+        hourInc.setOnClickListener(
+                new Button.OnClickListener(){
+                    //increment hours count
+                    public void onClick(View view)
+                    {
+                        if(hours==12)
+                        {
+                            hours=1;
+                        }
+                        else
+                        {
+                            ++hours;
+                        }
+                        displayHours(hours);
+                    }
+                }
+        );
+
+        /*
+        Minutes Button methods
+         */
+        minDec.setOnClickListener(
+                new Button.OnClickListener(){
+                    //decrement mins count
+                    public  void onClick(View view)
+                    {
+                        if(mins==0)
+                        {
+                            mins=55;
+                        }
+                        else
+                        {
+                            mins-=5;
+                        }
+                        displayMins(mins);
+                    }
+                }
+        );
+
+
+        minInc.setOnClickListener(
+                new Button.OnClickListener(){
+                    //increment mins count
+                    public void onClick(View view)
+                    {
+                        if(mins==55)
+                        {
+                            mins=0;
+                        }
+                        else
+                        {
+                            mins+=5;
+                        }
+                        displayMins(mins);
+                    }
+                }
+        );
+        /**
+         * Methods for the AM/PM
+         */
+        amPm.setOnClickListener(
+                new Button.OnClickListener(){
+                    public void onClick(View view)
+                    {
+                        period*=-1;
+                        Button button=(Button) findViewById(R.id.amPmButton);
+                        if(period==1)
+                            button.setText("AM");
+                        else
+                            button.setText("PM");
+                    }
+
+
+                }
+        );
+
+
+
+    }
+    //--------------------------------------------------------------------------------
+
+
 
 
     /**
@@ -51,33 +181,9 @@ public class MainActivity extends AppCompatActivity {
             hourText.setText("0"+x);
     }
 
-    //decrement hours count
-    public  void hourDec(View view)
-    {
-        if(hours==1)
-        {
-            hours=12;
-        }
-        else
-        {
-            --hours;
-        }
-        displayHours(hours);
-    }
 
-    //increment hours count
-    public void hourInc(View view)
-    {
-        if(hours==12)
-        {
-            hours=1;
-        }
-        else
-        {
-            ++hours;
-        }
-        displayHours(hours);
-    }
+
+
 
 
     //-------------------------------------------------------------------
@@ -97,47 +203,12 @@ public class MainActivity extends AppCompatActivity {
             minText.setText("0"+x);
     }
 
-    //decrement mins count
-    public  void minDec(View view)
-    {
-        if(mins==0)
-        {
-            mins=55;
-        }
-        else
-        {
-            mins-=5;
-        }
-        displayMins(mins);
-    }
 
-    //increment mins count
-    public void minInc(View view)
-    {
-        if(mins==55)
-        {
-            mins=0;
-        }
-        else
-        {
-            mins+=5;
-        }
-        displayMins(mins);
-    }
+
+
 
     //----------------------------------------------------
 
-    /**
-     * Methods for the AM/PM
-     */
-    public void amPmButton(View view)
-    {
-        period*=-1;
-        Button button=(Button) findViewById(R.id.amPmButton);
-        if(period==1)
-            button.setText("AM");
-        else
-            button.setText("PM");
-    }
+
 
 }
